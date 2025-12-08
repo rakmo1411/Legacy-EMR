f7aaf9709f7cecc47057556c1e655b31cf3cfa7a
rakmo1411 <rakmo1411@gmail.com>
2025-12-08 11:56:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Notice the extra parentheses required here: (({ mode }) => ({ ... }))
 export default defineConfig(({ mode }) => ({
-
-      plugins: [react()],
-      
-});
+  plugins: [react()],
+  // You can now use 'mode' here if needed
+}));
